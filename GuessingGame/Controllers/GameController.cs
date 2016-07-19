8dd0872001e9139ee71ea2db448e799f6862bfc4
Jennifer <jrlawyer@gmail.com>
2016-07-18 20:48:45 -0400

    Initial commit.

---
+++ GuessingGame/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using GuessingGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            Session["Answer"] = new Random().Next(1, 10);
+
+            return View();
+        }
+
+        private bool GuessWasCorrect(int guess)
+        {
+            return guess == (int)Session["Answer"];
+        }
+
+        
+        [HttpPost]
+        public ActionResult Index(GameModel model)
+        {
+            ViewBag.Win = GuessWasCorrect(model.Guess);
+
+            return View(model);
+        }
+    }
+}
---
+++ GuessingGame/Models/GameModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class GameModel
+    {
+        [Display(Name ="Enter Your Name")]
+        public string PlayerName { get; set; }
+
+        [Display(Name = "What is Your Guess?")]
+        public int Guess { get; set; }
+
+        public override string ToString()
+        {
+            return $"{PlayerName} ({Guess})";
+        }
+    }
+}
